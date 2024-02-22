fc8d946803bb4b6c680c63510ff56bc7c469180b
sws000171 <higuchi@software-s.co.jp>
2024-02-22 21:54:54 +0900

    2/22

--- src/App.vue
+++ src/App.vue
@@ -57,7 +57,7 @@ provide('appDeleteFlg', appDeleteFlg);
     <div class="modal-dialog">
       <div class="modal-content">
         <div class="modal-header">
-          <h1 class="modal-title fs-5" id="deleteModalLabel">Modal title</h1>
+          <h1 class="modal-title fs-5" id="deleteModalLabel">Delete Diary</h1>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
